da00a20a8099be78f3258ad3dafa157322087dab
jorge jaramillo <albeiro.jaramillo@est.edu.iudigital.edu.co>
2025-06-10 13:11:12 -0500

    inicial commit, se crea la clase Animal se crea el constructor de la clase Animal

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,3 +2,18 @@ public class Animal {
     private String nombre;
     private String tipo; // terrestre, aéreo, acuático
     private String genero; // masculino, femenino
+    // se crea el contructor de la clase Animal
+    public Animal(String nombre, String tipo, String genero) {
+        this.nombre = nombre;
+        this.tipo = tipo.toLowerCase();
+        this.genero = genero.toLowerCase();
+    }
+
+    public String getNombre() { return nombre; }
+    public String getTipo() { return tipo; }
+    public String getGenero() { return genero; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+    public void setGenero(String genero) { this.genero = genero; }
+}
