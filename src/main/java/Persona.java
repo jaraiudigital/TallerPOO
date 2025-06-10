3496efff389120d129146bf7b5d85377ca5dc119
jorge jaramillo <albeiro.jaramillo@est.edu.iudigital.edu.co>
2025-06-10 13:18:09 -0500

    inicial commit, se crea la clase Animal se crea el constructor de la clase Animal se crean los metodos getter y setter de la clase Animal se crea la clase Persona y atributos

---
+++ src/main/java/Persona.java
@@ -0,0 +1,8 @@
+// clase persona y atributos
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero; // M o F
+    private double sueldoHora;
+    private String cargo;
